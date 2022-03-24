f1097aaf8d1b8b94976b46b109fc8e3648b7b0e7
c9u11 <tjrdud6412@naver.com>
2022-03-24 23:13:59 +0900

    백트래킹 : 2580 (시간초과)

--- test.js
+++ test.js
@@ -14,31 +14,57 @@ r.on("line", function (line) {
 });
 
 function main(input) {
-  const num = Number(input[0]);
-  const rows = new Array(num + 1).fill(0).map((el) => 0);
+  const board = input.map((v) => v.split(" ").map((v) => +v));
+  let checkList = [];
 
-  const isAble = (row) => {
-    for (let i = 1; i < row; i++) {
-      if (rows[i] === rows[row] || Math.abs(rows[row] - rows[i]) === row - i) {
-        return false;
+  // 9x9 Temp List
+  for (let x = 0; x < 9; x++) {
+    for (let y = 0; y < 9; y++) {
+      if (board[x][y] === 0) checkList.push(`${x} ${y}`);
+    }
+  }
+
+  sudoku();
+
+  function sudoku() {
+    // 다찾으면 종료
+    if (!checkList.length) {
+      console.log(board.map((v) => v.join(" ")).join("\n"));
+      process.exit(0);
+    }
+
+    const [findX, findY] = checkList
+      .pop()
+      .split(" ")
+      .map((v) => +v);
+
+    for (let i = 1; i <= 9; i++) {
+      if (findNum(findX, findY, i)) {
+        board[findX][findY] = i;
+        sudoku();
+        board[findX][findY] = 0;
+      }
+    }
+    checkList.push(`${findX} ${findY}`);
+  }
+
+  function findNum(findX, findY, value) {
+    // x 고정하고 찾기
+    for (let y = 0; y < 9; y++) {
+      if (board[findX][y] === value) return false;
+    }
+    // y 고정하고 찾기
+    for (let x = 0; x < 9; x++) {
+      if (board[x][findY] === value) return false;
+    }
+    // 3*3 안에서 찾기
+    const rangeX = Math.floor(findX / 3) * 3,
+      rangeY = Math.floor(findY / 3) * 3;
+    for (let x = rangeX; x < rangeX + 3; x++) {
+      for (let y = rangeY; y < rangeY + 3; y++) {
+        if (board[x][y] === value) return false;
       }
     }
     return true;
-  };
-
-  let count = 0;
-  const queen = (numOfQueen) => {
-    if (isAble(numOfQueen)) {
-      if (numOfQueen === num) {
-        count++;
-      } else {
-        for (let i = 1; i <= num; i++) {
-          rows[numOfQueen + 1] = i;
-          queen(numOfQueen + 1);
-        }
-      }
-    }
-  };
-  queen(0);
-  console.log(count);
+  }
 }
