5c14dd9eb79783018c5394941917e22752ce6eff
c9u11 <tjrdud6412@naver.com>
2022-06-05 21:24:47 +0900

    기하1 : 2477
    - 초안

--- test.js
+++ test.js
@@ -15,23 +15,32 @@ r.on("line", function (line) {
 });
 
 function main(input) {
-  const [W, H, X, Y, P] = input[0].split(" ").map(Number);
-  let result = 0;
-  for (let i = 1; i <= P; i++) {
-    const [pX, pY] = input[i].split(" ").map(Number);
-    if (isIn(pX, pY)) result++;
-  }
-  console.log(result);
-
-  function isIn(pX, pY) {
-    if (Y <= pY && pY <= Y + H) {
-      if (X <= pX && pX <= X + W) return true;
-      else {
-        const xLen = Math.abs((X > pX ? X : X + W) - pX);
-        const yLen = Math.abs(Y + H / 2 - pY);
-        if (H / 2 >= Math.sqrt(xLen * xLen + yLen * yLen)) return true;
-        else return false;
-      }
+  const unit = +input[0];
+  let max = 0,
+    maxIndex = 0;
+  for (let i = 1; i <= 6; i++) {
+    const num = +input[i].split(" ")[1];
+    if (max < num) {
+      max = num;
+      maxIndex = i;
     }
+  }
+  const prevIndex = maxIndex > 1 ? maxIndex - 1 : 6;
+  const nextIndex = maxIndex < 7 ? maxIndex + 1 : 1;
+  const prev = +input[prevIndex].split(" ")[1];
+  const next = +input[nextIndex].split(" ")[1];
+  console.log(prev, next, prevIndex, nextIndex);
+  if (prev > next) {
+    const area = max * prev;
+    const minusArea =
+      +input[prevIndex < 3 ? prevIndex + 4 : prevIndex - 2].split(" ")[1] *
+      +input[maxIndex > 4 ? maxIndex - 4 : maxIndex + 2].split(" ")[1];
+    console.log((area - minusArea) * unit);
+  } else {
+    const area = max * next;
+    const minusArea =
+      +input[maxIndex < 3 ? maxIndex + 4 : maxIndex - 2].split(" ")[1] *
+      +input[nextIndex > 4 ? nextIndex - 4 : nextIndex + 2].split(" ")[1];
+    console.log((area - minusArea) * unit);
   }
 }
