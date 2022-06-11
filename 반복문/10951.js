8bb0f68125c75fc6b83647dbd39e6711cf88d195
c9u11 <tjrdud6412@naver.com>
2022-06-11 23:56:39 +0900

    반복문 : 10951

---
+++ 반복문/10951.js
@@ -0,0 +1,19 @@
+const { mainModule } = require("process");
+var readline = require("readline");
+var r = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+r.on("line", function (line) {
+  main(line);
+});
+r.on("close", function () {
+  process.exit();
+});
+
+function main(input) {
+  const A = input.split(" ")[0] * 1;
+  const B = input.split(" ")[1] * 1;
+  // if (!(A || B)) r.close();
+  console.log(A + B);
+}
