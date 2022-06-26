263861dd0b6b096a73be104aebe772174a213e3f
c9u11 <tjrdud6412@naver.com>
2022-06-26 23:55:51 +0900

    정수론 및 조합론 : 2981
    - 경우의 수 계산하는 부분 수정 필요

--- test.js
+++ test.js
@@ -15,25 +15,30 @@ r.on("line", function (line) {
 });
 
 function main(input) {
-  const numLen = +input.shift();
-  const numArray = input.map(Number);
+  let index = 1;
   const result = [];
-
-  numArray.sort();
-
-  let GCD = numArray[1] - numArray[0];
-
-  for (let i = 2; i < numLen; i++) {
-    GCD = findGCD(GCD, numArray[i] - numArray[i - 1]);
+  const testCaseNum = +input[0];
+  for (let i = 1; i <= testCaseNum; i++) {
+    let cnt = 0;
+    const dressNum = +input[index];
+    const dressMap = {};
+    for (let j = index + 1; j < index + 1 + dressNum; j++) {
+      const type = input[j].split(" ")[1];
+      if (!dressMap[type]) dressMap[type] = 1;
+      else dressMap[type]++;
+    }
+    const typeArray = Object.keys(dressMap);
+    for (let j = 0; j < typeArray.length; j++) {
+      for (let k = 0; k < typeArray.length - j; k++) {
+        let tempCnt = 1;
+        for (let l = 0; l < j + 1; l++) {
+          tempCnt *= dressMap[typeArray[k + l]];
+        }
+        cnt += tempCnt;
+      }
+    }
+    index += dressNum + 1;
+    result.push(cnt);
   }
-
-  for (let i = 2; i <= GCD; i++) {
-    if (GCD % i === 0) result.push(i);
-  }
-  console.log(result.join(" "));
-
-  function findGCD(a, b) {
-    if (!b) return a;
-    else return findGCD(b, a % b);
-  }
+  console.log(result.join("\n"));
 }
