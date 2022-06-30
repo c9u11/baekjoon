be57709c35a83fe2f9fca67895a51dc1043e7b98
c9u11 <tjrdud6412@naver.com>
2022-06-30 21:30:23 +0900

    백트래킹 : 2580

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 var readline = require("readline");
-const { resourceLimits } = require("worker_threads");
 var r = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -15,19 +14,53 @@ r.on("line", function (line) {
 });
 
 function main(input) {
-  const [N, M] = input[0].split(" ").map(Number);
+  const board = input.map((v) => v.split(" ").map(Number));
+  let checkList = [];
 
-  const two = getPower(N, 2) - getPower(N - M, 2) - getPower(M, 2);
-  const five = getPower(N, 5) - getPower(N - M, 5) - getPower(M, 5);
-
-  console.log(two > five ? five : two);
-
-  function getPower(num, power) {
-    let cnt = 0;
-    while (num >= power) {
-      num /= power;
-      cnt += Math.floor(num);
+  // 9x9 Temp List
+  for (let x = 0; x < 9; x++) {
+    for (let y = 0; y < 9; y++) {
+      if (board[x][y] === 0) checkList.push([x, y]);
     }
-    return cnt;
+  }
+
+  sudoku(0);
+
+  function sudoku(cnt) {
+    // 다찾으면 종료
+    if (cnt === checkList.length) {
+      console.log(board.map((v) => v.join(" ")).join("\n"));
+      process.exit(0);
+    }
+
+    const [findX, findY] = checkList[cnt];
+
+    for (let i = 1; i <= 9; i++) {
+      if (findNum(findX, findY, i)) {
+        board[findX][findY] = i;
+        sudoku(cnt + 1);
+        board[findX][findY] = 0;
+      }
+    }
+  }
+
+  function findNum(findX, findY, value) {
+    // x 고정하고 찾기
+    for (let y = 0; y < 9; y++) {
+      if (board[findX][y] === value) return false;
+    }
+    // y 고정하고 찾기
+    for (let x = 0; x < 9; x++) {
+      if (board[x][findY] === value) return false;
+    }
+    // 3*3 안에서 찾기
+    const rangeX = Math.floor(findX / 3) * 3,
+      rangeY = Math.floor(findY / 3) * 3;
+    for (let x = rangeX; x < rangeX + 3; x++) {
+      for (let y = rangeY; y < rangeY + 3; y++) {
+        if (board[x][y] === value) return false;
+      }
+    }
+    return true;
   }
 }
--- 백트래킹/2580.js
+++ 백트래킹/2580.js
@@ -14,38 +14,34 @@ r.on("line", function (line) {
 });
 
 function main(input) {
-  const board = input.map((v) => v.split(" ").map((v) => +v));
+  const board = input.map((v) => v.split(" ").map(Number));
   let checkList = [];
 
   // 9x9 Temp List
   for (let x = 0; x < 9; x++) {
     for (let y = 0; y < 9; y++) {
-      if (board[x][y] === 0) checkList.push(`${x} ${y}`);
+      if (board[x][y] === 0) checkList.push([x, y]);
     }
   }
 
-  sudoku();
+  sudoku(0);
 
-  function sudoku() {
+  function sudoku(cnt) {
     // 다찾으면 종료
-    if (!checkList.length) {
+    if (cnt === checkList.length) {
       console.log(board.map((v) => v.join(" ")).join("\n"));
       process.exit(0);
     }
 
-    const [findX, findY] = checkList
-      .pop()
-      .split(" ")
-      .map((v) => +v);
+    const [findX, findY] = checkList[cnt];
 
     for (let i = 1; i <= 9; i++) {
       if (findNum(findX, findY, i)) {
         board[findX][findY] = i;
-        sudoku();
+        sudoku(cnt + 1);
         board[findX][findY] = 0;
       }
     }
-    checkList.push(`${findX} ${findY}`);
   }
 
   function findNum(findX, findY, value) {
