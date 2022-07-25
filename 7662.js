01ce2be29ed3220c161734edff2dddaa73c67514
c9u11 <tjrdud6412@naver.com>
2022-07-25 14:57:24 +0900

    이중 우선순위 큐 : 7662

--- 7662.js
+++ 7662.js
@@ -14,31 +14,173 @@ r.on("line", function (line) {
 });
 
 function main(input) {
-  const tempArray = [];
-  let end = 0;
-  for (let i = 1; i < input.length; i++) {
-    if (isNaN(+input[i])) {
-      const alpha = input[i].split(" ")[0];
-      const num = +input[i].split(" ")[1];
-      if (alpha === "D") {
-        tempArray.splice(num === -1 ? 0 : tempArray.length - 1, 1);
-      } else {
-        let temp = null;
-        for (let j = 0; j < tempArray.length; j++) {
-          if (tempArray[j] < num) temp = j + 1;
-        }
-        tempArray.splice(temp === null ? tempArray.length - 1 : temp, 0, num);
-      }
-      if (i === end) {
-        if (tempArray.length) {
-          console.log(`${tempArray[tempArray.length - 1]} ${tempArray[0]}`);
-        } else {
-          console.log("EMPTY");
-        }
-        tempArray.splice(0, tempArray.length - 1);
-      }
-    } else {
-      end += +input[i] + 1;
+  const result = [];
+  let cnt = 0;
+  let heap;
+  input.forEach((v, i) => {
+    if (!i) return;
+    if (!cnt) {
+      heap = new Heap();
+      cnt = +v;
+      return;
     }
+    cnt--;
+    const [oper, val] = v.split(" ");
+    if (oper === "I") heap.push(+val);
+    else heap.pop(+val);
+    if (!cnt) {
+      if (heap) result.push(heap.getMinMax());
+    }
+  });
+  console.log(result.join("\n"));
+}
+
+class Heap {
+  constructor() {
+    this.maxHeap = [null];
+    this.minHeap = [null];
+    this.exist = [];
+  }
+  getMinMax() {
+    let min, max;
+    while (min === undefined) {
+      if (this.minHeap.length !== 1 && !this.exist[this.minHeap[1]])
+        this.minPop();
+      else min = this.minHeap[1] ?? 0;
+    }
+    while (max === undefined) {
+      if (this.maxHeap.length !== 1 && !this.exist[this.maxHeap[1]])
+        this.maxPop();
+      else max = this.maxHeap[1] ?? 0;
+    }
+    return this.minHeap.length === 1 && this.maxHeap.length === 1
+      ? "EMPTY"
+      : `${max} ${min}`;
+  }
+  push(val) {
+    this.maxPush(val);
+    this.minPush(val);
+    this.exist[val] = this.exist[val] ? this.exist[val] + 1 : 1;
+  }
+  pop(val) {
+    let output = undefined;
+    while (output === undefined) {
+      if (val === 1) {
+        const temp = this.maxPop();
+        if (temp === 0) output = 0;
+        else if (this.exist[temp]) {
+          this.exist[temp]--;
+          output = temp;
+        }
+      } else if (val === -1) {
+        const temp = this.minPop();
+        if (temp === 0) output = 0;
+        else if (this.exist[temp]) {
+          this.exist[temp]--;
+          output = temp;
+        }
+      }
+    }
+    return output;
+  }
+  minPush(val) {
+    this.minHeap.push(val);
+    let curIdx = this.minHeap.length - 1;
+    let parIdx = Math.floor(curIdx / 2);
+
+    while (curIdx > 1 && this.minHeap[parIdx] > this.minHeap[curIdx]) {
+      [this.minHeap[parIdx], this.minHeap[curIdx]] = [
+        this.minHeap[curIdx],
+        this.minHeap[parIdx],
+      ];
+      curIdx = parIdx;
+      parIdx = Math.floor(curIdx / 2);
+    }
+  }
+  minPop() {
+    const min = this.minHeap[1] ?? 0;
+    if (this.minHeap.length <= 2) this.minHeap = [null];
+    else this.minHeap[1] = this.minHeap.pop();
+
+    let curIdx = 1;
+    let leftIdx = curIdx * 2;
+    let rightIdx = curIdx * 2 + 1;
+
+    if (!this.minHeap[leftIdx]) return min;
+    if (!this.minHeap[rightIdx]) {
+      if (this.minHeap[leftIdx] < this.minHeap[curIdx]) {
+        [this.minHeap[leftIdx], this.minHeap[curIdx]] = [
+          this.minHeap[curIdx],
+          this.minHeap[leftIdx],
+        ];
+      }
+      return min;
+    }
+
+    while (
+      this.minHeap[leftIdx] < this.minHeap[curIdx] ||
+      this.minHeap[rightIdx] < this.minHeap[curIdx]
+    ) {
+      const minIdx =
+        this.minHeap[leftIdx] > this.minHeap[rightIdx] ? rightIdx : leftIdx;
+      [this.minHeap[minIdx], this.minHeap[curIdx]] = [
+        this.minHeap[curIdx],
+        this.minHeap[minIdx],
+      ];
+      curIdx = minIdx;
+      leftIdx = curIdx * 2;
+      rightIdx = curIdx * 2 + 1;
+    }
+    return min;
+  }
+  maxPush(val) {
+    this.maxHeap.push(val);
+    let curIdx = this.maxHeap.length - 1;
+    let parIdx = Math.floor(curIdx / 2);
+
+    while (curIdx > 1 && this.maxHeap[parIdx] < this.maxHeap[curIdx]) {
+      [this.maxHeap[parIdx], this.maxHeap[curIdx]] = [
+        this.maxHeap[curIdx],
+        this.maxHeap[parIdx],
+      ];
+      curIdx = parIdx;
+      parIdx = Math.floor(curIdx / 2);
+    }
+  }
+  maxPop() {
+    const min = this.maxHeap[1] ?? 0;
+    if (this.maxHeap.length <= 2) this.maxHeap = [null];
+    else this.maxHeap[1] = this.maxHeap.pop();
+
+    let curIdx = 1;
+    let leftIdx = curIdx * 2;
+    let rightIdx = curIdx * 2 + 1;
+
+    if (!this.maxHeap[leftIdx]) return min;
+    if (!this.maxHeap[rightIdx]) {
+      if (this.maxHeap[leftIdx] > this.maxHeap[curIdx]) {
+        [this.maxHeap[leftIdx], this.maxHeap[curIdx]] = [
+          this.maxHeap[curIdx],
+          this.maxHeap[leftIdx],
+        ];
+      }
+      return min;
+    }
+
+    while (
+      this.maxHeap[leftIdx] > this.maxHeap[curIdx] ||
+      this.maxHeap[rightIdx] > this.maxHeap[curIdx]
+    ) {
+      const minIdx =
+        this.maxHeap[leftIdx] < this.maxHeap[rightIdx] ? rightIdx : leftIdx;
+      [this.maxHeap[minIdx], this.maxHeap[curIdx]] = [
+        this.maxHeap[curIdx],
+        this.maxHeap[minIdx],
+      ];
+      curIdx = minIdx;
+      leftIdx = curIdx * 2;
+      rightIdx = curIdx * 2 + 1;
+    }
+    return min;
   }
 }
