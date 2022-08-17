c206ba28401708bcbd297570191c20ba3b18fd37
c9u11 <tjrdud6412@naver.com>
2022-08-17 16:01:40 +0900

    DSLR : 9019
    - 시간 줄임

--- 9019.js
+++ 9019.js
@@ -1,60 +1,50 @@
-var readline = require("readline");
-var r = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-let input = [];
-
-r.on("line", function (line) {
-  input.push(line);
-}).on("close", function () {
-  main(input);
-  process.exit();
-});
-
-function main(input) {
-  const result = new Array(+input[0]);
-  for (let i = 1; i <= +input[0]; i++) {
-    const [A, B] = input[i].split(" ").map(Number);
-    const dslr = DSLR(A);
-    let visited = new Array(10000);
-    visited[dslr[0]] = "D";
-    visited[dslr[1]] = "S";
-    visited[dslr[2]] = "L";
-    visited[dslr[3]] = "R";
-    let [queue, tempQueue] = [dslr, []];
-    while (queue.length && !visited[B]) {
-      for (let i = 0; i < queue.length; i++) {
-        const [d, s, l, r] = DSLR(queue[i]);
-        if (!visited[d]) {
-          visited[d] = visited[queue[i]] + "D";
-          tempQueue[tempQueue.length] = d;
-        }
-        if (!visited[s]) {
-          visited[s] = visited[queue[i]] + "S";
-          tempQueue[tempQueue.length] = s;
-        }
-        if (!visited[l]) {
-          visited[l] = visited[queue[i]] + "L";
-          tempQueue[tempQueue.length] = l;
-        }
-        if (!visited[r]) {
-          visited[r] = visited[queue[i]] + "R";
-          tempQueue[tempQueue.length] = r;
-        }
+const result = [];
+for (let i = 1; i <= +input[0]; i++) {
+  const [A, B] = input[i].split(" ").map(Number);
+  const dslr = DSLR(A);
+  let visited = new Array(10000);
+  visited[dslr[0]] = "D";
+  visited[dslr[1]] = "S";
+  visited[dslr[2]] = "L";
+  visited[dslr[3]] = "R";
+  let [queue, tempQueue] = [dslr, []];
+  while (queue.length && !visited[B]) {
+    for (let i = 0; i < queue.length; i++) {
+      const [d, s, l, r] = DSLR(queue[i]);
+      if (!visited[d]) {
+        visited[d] = visited[queue[i]] + "D";
+        tempQueue.push(d);
       }
-      [queue, tempQueue] = [tempQueue, []];
+      if (!visited[s]) {
+        visited[s] = visited[queue[i]] + "S";
+        tempQueue.push(s);
+      }
+      if (!visited[l]) {
+        visited[l] = visited[queue[i]] + "L";
+        tempQueue.push(l);
+      }
+      if (!visited[r]) {
+        visited[r] = visited[queue[i]] + "R";
+        tempQueue.push(r);
+      }
     }
-    result[i - 1] = visited[B];
+    [queue, tempQueue] = [tempQueue, []];
   }
-  console.log(result.join("\n"));
+  result.push(visited[B]);
 }
+console.log(result.join("\n"));
 
 function DSLR(num) {
   const D = (num * 2) % 10000;
   const S = !num ? 9999 : +num - 1;
-  const L = (num % 1000) * 10 + Math.floor(num / 1000);
-  const R = (num % 10) * 1000 + Math.floor(num / 10);
+  const one = Math.floor((num / 1000) % 10);
+  const two = Math.floor((num / 100) % 10);
+  const three = Math.floor((num / 10) % 10);
+  const four = Math.floor((num / 1) % 10);
+  const L = two * 1000 + three * 100 + four * 10 + one;
+  const R = four * 1000 + one * 100 + two * 10 + three;
   return [D, S, L, R];
 }
